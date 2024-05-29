19f9a62db607d67796165cc85d26dcb1520b954b
chotty <kart.71772118120@gct.ac.in>
2024-05-29 20:34:54 +0530

    Fix file paths and JavaScript errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,91 +10,89 @@ function myMenuFunction(){
 }
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-
-
-
+window.addEventListener('scroll', function() {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 0) {
+    nav.classList.add('shadow');
+  } else {
+    nav.classList.remove('shadow');
+  }
+});
 
 /* ----- TYPING EFFECT ----- */
-var typingEffect = new Typed(".typedText",{
-  strings : ["UI UX Designer","Developer"],
-  loop : true,
-  typeSpeed : 100, 
-  backSpeed : 80,
-  backDelay : 2000
-})
-
+var typingEffect = new Typed(".typedText", {
+  strings: ["UI UX Designer", "Developer"],
+  loop: true,
+  typeSpeed: 100, 
+  backSpeed: 80,
+  backDelay: 2000
+});
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
 const sr = ScrollReveal({
-      origin: 'top',
-      distance: '80px',
-      duration: 2000,
-      reset: true     
-})
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true     
+});
 
 /* -- HOME -- */
-sr.reveal('.featured-text-card',{})
-sr.reveal('.featured-name',{delay: 100})
-sr.reveal('.featured-text-info',{delay: 200})
-sr.reveal('.featured-text-btn',{delay: 200})
-sr.reveal('.social_icons',{delay: 200})
-sr.reveal('.featured-image',{delay: 300})
+sr.reveal('.featured-text-card', {});
+sr.reveal('.featured-name', { delay: 100 });
+sr.reveal('.featured-text-info', { delay: 200 });
+sr.reveal('.featured-text-btn', { delay: 200 });
+sr.reveal('.social_icons', { delay: 200 });
 
+sr.reveal('.featured-image', { delay: 300 });
 
 /* -- PROJECT BOX -- */
-sr.reveal('.project-box',{interval: 200})
+sr.reveal('.project-box', { interval: 200 });
 
 /* -- HEADINGS -- */
-sr.reveal('.top-header',{})
+sr.reveal('.top-header', {});
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
-origin: 'left',
-distance: '80px',
-duration: 2000,
-reset: true
-})
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
 
-srLeft.reveal('.about-info',{delay: 100})
-srLeft.reveal('.contact-info',{delay: 100})
+srLeft.reveal('.about-info', { delay: 100 });
+srLeft.reveal('.contact-info', { delay: 100 });
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
-origin: 'right',
-distance: '80px',
-duration: 2000,
-reset: true
-})
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
 
-srRight.reveal('.skills-box',{delay: 100})
-srRight.reveal('.form-control',{delay: 100})
-
-
+srRight.reveal('.skills-box', { delay: 100 });
+srRight.reveal('.form-control', { delay: 100 });
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
-const scrollY = window.scrollY;
+  const scrollY = window.scrollY;
 
-sections.forEach(current =>{
-  const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 50,
-    sectionId = current.getAttribute('id')
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
 
-  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
-
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-
-  }  else {
-
-    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-
-  }
-})
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  });
 }
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive);
